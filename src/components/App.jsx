3383e872833b6ff99a37a03c19d6ea4b4fb96bcd
Ryan Yonek <ryanyonek@gmail.com>
2025-12-17 11:23:43 -0500

    Initial React Components, Vexflow Test

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import VexFlowSheet from "./VexFlowSheet";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Sheet Music</h1>
+      <VexFlowSheet />
+    </div>
+  );
+}
+
+export default App;
